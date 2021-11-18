40bf8bd6cf8140dd24dd5a6e66aedc75f59dab67
Jarikkomarik <ufland.ufland@gmail.com>
2021-11-18 18:37:01 +0100

    Fixed bug with 0 value in generateAbilityValue().

---
+++ src/main/java/com/github/jarikkomarik/DndCharacter.java
@@ -0,0 +1,44 @@
+package com.github.jarikkomarik;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+@ToString
+@Getter
+public class DndCharacter {
+    @ToString.Exclude
+    private Random r = new Random();
+
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private int hitPoint;
+
+    public DndCharacter(){
+        strength = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        dexterity = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        constitution = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        intelligence = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        wisdom = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        charisma = generateAbilityValue(r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1,r.nextInt(6)+1);
+        hitPoint = calculateModifier(constitution);
+    }
+
+    public int calculateModifier(int constitution) {
+        return 10 + (int)Math.floor(((double) constitution - 10) / 2);
+    }
+
+    public int generateAbilityValue(int a, int b, int c, int d) {
+        return Arrays.stream(new int []{a,b,c,d})
+                .sorted()
+                .skip(1)
+                .sum();
+    }
+}
--- src/test/java/DndCharacterTest.java
+++ src/test/java/DndCharacterTest.java
@@ -1,91 +1,62 @@
+import com.github.jarikkomarik.DndCharacter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DndCharacterTest {
     @Test
-    @DisplayName("modifier of 0 = -5")
-    void modifierTest0(){
-        Assertions.assertEquals(-5,DndCharacter.modifier(0));
+    void calculateModifier_shouldReturnNeg5_whenConstitutionIs0(){                                                      //testing calculateModifier()
+        Assertions.assertEquals(6, new DndCharacter().calculateModifier(3));
+    }
+
+    @Test
+    void calculateModifier_shouldReturnNeg1_whenConstitutionIs9(){
+        Assertions.assertEquals(9,new DndCharacter().calculateModifier(9));
     }
     @Test
-    @DisplayName("modifier of 1 = -5")
-    void modifierTest1(){
-        Assertions.assertEquals(-5,DndCharacter.modifier(1));
+    void calculateModifier_shouldReturn0_whenConstitutionIs10(){
+        Assertions.assertEquals(10,new DndCharacter().calculateModifier(10));
+    }
+
+    @Test
+    void calculateModifier_shouldReturn3_whenConstitutionIs16(){
+        Assertions.assertEquals(14,new DndCharacter().calculateModifier(18));
+    }
+
+    @Test
+    void generateAbilityValue_shouldReturn3_when_1111(){                                                                //testing generateAbilityValue()
+        Assertions.assertEquals(3,new DndCharacter().generateAbilityValue(1,1,1,1));
     }
     @Test
-    @DisplayName("modifier of 2 = -4")
-    void modifierTest2(){
-        Assertions.assertEquals(-4,DndCharacter.modifier(2));
+    void generateAbilityValue_shouldReturn18_when_6666(){
+        Assertions.assertEquals(18,new DndCharacter().generateAbilityValue(6,6,6,6));
     }
     @Test
-    @DisplayName("modifier of 3 = -4")
-    void modifierTest3(){
-        Assertions.assertEquals(-4,DndCharacter.modifier(3));
+    void generateAbilityValue_shouldReturn14_when_8421(){
+        Assertions.assertEquals(14,new DndCharacter().generateAbilityValue(8,4,2,1));
     }
+
     @Test
-    @DisplayName("modifier of 4 = -3")
-    void modifierTest4(){
-        Assertions.assertEquals(-3,DndCharacter.modifier(4));
+    void generateAbilityValue_shouldReturn16_when_4466(){
+        Assertions.assertEquals(16,new DndCharacter().generateAbilityValue(4,4,6,6));
     }
+
     @Test
-    @DisplayName("modifier of 5 = -3")
-    void modifierTest5(){
-        Assertions.assertEquals(-3,DndCharacter.modifier(5));
-    }
-    @Test
-    @DisplayName("modifier of 6 = -2")
-    void modifierTest6(){
-        Assertions.assertEquals(-2,DndCharacter.modifier(6));
-    }
-    @Test
-    @DisplayName("modifier of 7 = -2")
-    void modifierTest7(){
-        Assertions.assertEquals(-2,DndCharacter.modifier(7));
-    }
-    @Test
-    @DisplayName("modifier of 8 = -1")
-    void modifierTest8(){
-        Assertions.assertEquals(-1,DndCharacter.modifier(8));
-    }
-    @Test
-    @DisplayName("modifier of 9 = -1")
-    void modifierTest9(){
-        Assertions.assertEquals(-1,DndCharacter.modifier(9));
-    }
-    @Test
-    @DisplayName("modifier of 10 = 0")
-    void modifierTest10(){
-        Assertions.assertEquals(0,DndCharacter.modifier(10));
-    }
-    @Test
-    @DisplayName("modifier of 11 = 0")
-    void modifierTest11(){
-        Assertions.assertEquals(0,DndCharacter.modifier(11));
-    }
-    @Test
-    @DisplayName("modifier of 12 = 1")
-    void modifierTest12(){
-        Assertions.assertEquals(1,DndCharacter.modifier(12));
-    }
-    @Test
-    @DisplayName("modifier of 13 = 1")
-    void modifierTest13(){
-        Assertions.assertEquals(1,DndCharacter.modifier(13));
-    }
-    @Test
-    @DisplayName("modifier of 14 = 2")
-    void modifierTest14(){
-        Assertions.assertEquals(2,DndCharacter.modifier(14));
-    }
-    @Test
-    @DisplayName("modifier of 15 = 2")
-    void modifierTest15(){
-        Assertions.assertEquals(2,DndCharacter.modifier(15));
-    }
-    @Test
-    @DisplayName("modifier of 16 = 3")
-    void modifierTest16(){
-        Assertions.assertEquals(3,DndCharacter.modifier(16));
+    void constructor_shouldProduceValuesForVariables(){                                                                 //testing constructor()
+        DndCharacter testDndCharacter = new DndCharacter();
+
+        Assertions.assertTrue(testDndCharacter.getStrength() > 2 && testDndCharacter.getStrength() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getDexterity() > 2 && testDndCharacter.getDexterity() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getConstitution() > 2 && testDndCharacter.getConstitution() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getIntelligence() > 2 && testDndCharacter.getIntelligence() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getWisdom() > 2 && testDndCharacter.getWisdom() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getCharisma() > 2 && testDndCharacter.getCharisma() < 19);
+
+        Assertions.assertTrue(testDndCharacter.getHitPoint() > 5 && testDndCharacter.getHitPoint() < 15);
     }
 }
